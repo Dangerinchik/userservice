9539602d7e07418021f1791f01b91ea5a670bac6
Daniil Rainchik <danikrainchik2007@gmail.com>
2025-10-20 20:47:40 +0300

    Added repositories

---
+++ src/main/java/com/userservice/repository/CardInfoRepository.java
@@ -0,0 +1,35 @@
+package com.userservice.repository;
+
+import com.userservice.entity.CardInfo;
+import com.userservice.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface CardInfoRepository extends JpaRepository<CardInfo, Long> {
+
+    void createCardInfo(CardInfo cardInfo);
+
+    @Query(value = "SELECT * FROM card_info WHERE id = :id", nativeQuery = true)
+    Optional<CardInfo> getCardInfoById(@Param("id") Long id);
+
+    @Query("SELECT c FROM CardInfo c")
+    List<CardInfo> getAllCardsInfo();
+
+    @Modifying
+    @Query("UPDATE CardInfo c SET c.number = :#{#cardInfo.number}," +
+            " c.holder = :#{#cardInfo.holder}," +
+            " c.expirationDate = :#{#cardInfo.expirationDate}" +
+            " WHERE c.id = :id")
+    void updateCardInfoById(@Param("id") Long id,@Param("cardInfo") CardInfo cardInfo);
+
+    @Modifying
+    @Query(value = "DELETE FROM card_info WHERE id = :id", nativeQuery = true)
+    void deleteCardInfoById(@Param("id")Long id);
+}
---
+++ src/main/java/com/userservice/repository/UserRepository.java
@@ -0,0 +1,40 @@
+package com.userservice.repository;
+
+import com.userservice.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> createUser(User user);
+
+    @Query("SELECT u FROM User u WHERE u.id = :id")
+    Optional<User> getUserById(@Param("id") Long id);
+
+    @Query(value = "SELECT * FROM users", nativeQuery = true)
+    List<User> getAllUsers();
+
+    Optional<User> getUserByEmail(String email);
+
+    @Modifying
+    @Query(value = "UPDATE users SET" +
+            " name = :#{#user.name}," +
+            " surname = :#{#user.surname}," +
+            " birth_date = :#{#user.birthDate}," +
+            " email = :#{#user.email}" +
+            "" +
+            " WHERE id = :id", nativeQuery = true)
+    void updateUserById(@Param("id") Long id, @Param("user") User user);
+
+    @Modifying
+    @Query("DELETE FROM User u WHERE u.id = :id")
+    void deleteUserById(@Param("id") Long id);
+
+}
