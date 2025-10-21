4c0d0ca43908a558d9cfdc3377f6aa7866e0a9f3
Daniil Rainchik <danikrainchik2007@gmail.com>
2025-10-21 23:16:31 +0300

    Refactored createCardInfo method

--- src/main/java/com/userservice/service/CardInfoServiceImpl.java
+++ src/main/java/com/userservice/service/CardInfoServiceImpl.java
@@ -27,7 +27,7 @@ public class CardInfoServiceImpl implements CardInfoService {
 
     @Override
     public CardInfoDTO createCardInfo(CardInfoDTO cardInfoDTO) throws CardInfoAlreadyExistsException, CardInfoNotFoundException {
-        if(cardInfoRepository.existsByNumberHolderExpirationDate(cardInfoDTO.getNumber(),
+        if(cardInfoRepository.existsByNumberAndHolderAndExpirationDate(cardInfoDTO.getNumber(),
                 cardInfoDTO.getHolder(), cardInfoDTO.getExpirationDate())) {
             throw new CardInfoAlreadyExistsException("Card with these credentials already exists");
         }
